291c2d8f87e5033221e49aa81326a9d8112b1dd9
Niclass <niclas.soder@gmail.com>
2015-05-21 16:27:34 +0200

    Lagt in alla texturer, parallax, fixat menyer

---
+++ Assets/scripts/GameplayScripts/Parallax.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+
+    public moosecontroller characterController;
+ public float speedFactor = 1; 
+ Vector3 velocity;
+	
+	
+	void Update () 
+    {
+	    velocity.x = characterController.maxSpeed * speedFactor;
+        transform.Translate(velocity * Time.deltaTime);
+	}
+}
--- Assets/scripts/MenuScripts/GameOverScript.cs
+++ Assets/scripts/MenuScripts/GameOverScript.cs
@@ -6,10 +6,13 @@ public class GameOverScript : MonoBehaviour
 {
     public int score = 0;
     public int highScore = 0;
+    Text text;
     
     void Start()
     {
         //laddar in poängen spelaren fick innan game over
+        text = GetComponent<Text>();
+
         score = PlayerPrefs.GetInt("HighScore");
         score = PlayerPrefs.GetInt("Score");
     }
@@ -17,7 +20,12 @@ public class GameOverScript : MonoBehaviour
     void OnGUI()
     {
         //Ritar ut poäng
-        GUI.Label(new Rect(Screen.width / 2 - 40, 200, 280, 300), "<color=green><size=40>Score: </size></color>" + score);
-        GUI.Label(new Rect(Screen.width / 2 - 40, 400, 280, 300), "<color=green><size=40>High Score: </size></color>" + score);
+        GUI.Label(new Rect(Screen.width / 2 -80, Screen.height / 2 + 50, 280, 300), "<color=green><size=40>Score: </size></color>" + score);
+        //GUI.Label(new Rect(Screen.width / 2 - 40, 400, 280, 300), "<color=green><size=40>High Score: </size></color>" + score);
+    }
+
+    void Text()
+    {
+        text.text = "" + ((int)highScore);
     }
 }
