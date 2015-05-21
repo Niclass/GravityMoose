a78d022acbcaa1eee1cdacfb10d7c42ee4598446
Niclass <niclas.soder@gmail.com>
2015-05-21 17:13:17 +0200

    nya pick ups

--- Assets/scripts/GameplayScripts/Rotator.cs
+++ Assets/scripts/GameplayScripts/Rotator.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour
 {
-
+    
 
     void Update()
     {
         // gör så power ups roterar
-        transform.Rotate(new Vector3(45, 30, 60) * Time.deltaTime);
+        transform.Rotate(new Vector3(1, 1, 30) * Time.deltaTime);
+        
     }
 }
