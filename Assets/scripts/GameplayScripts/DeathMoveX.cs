09cd38337774d350cd185246fcc3a519e5cf1c99
sebbeekman <sebastianekman@live.com>
2015-05-24 01:43:09 +0200

    Fortsatt bana + DeathCubes kan röra sig i sidled
    Använda scritptet DeathMoveX för rörlse i sidled på deathcubes.

---
+++ Assets/scripts/GameplayScripts/DeathMoveX.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathMoveX : MonoBehaviour {
+	
+	protected Vector3 velocity;
+	public Transform transformm;
+	public float distance = 5f;
+	public float speed = 1f;
+	Vector3 originalPosition;
+	bool isGoingRight = false;
+	public float distFromStart;
+	public void Start()
+	{
+		originalPosition = gameObject.transform.position;
+		transformm = GetComponent<Transform>();
+		velocity = new Vector3(speed, 0, 0);
+		transformm.Translate(velocity.x * Time.deltaTime, 0, 0);
+	}
+	
+	void Update()
+	{
+		distFromStart = transformm.position.x - originalPosition.x;
+		
+		if (isGoingRight)
+		{
+			// Ändrar riktining åt vänster
+			if (distFromStart < -distance)
+			{
+				SwitchDirection();
+			}
+			transformm.Translate(-velocity.x * Time.deltaTime, 0, 0);
+		}
+		else
+		{
+			// ändrar riktning åt höger
+			if (distFromStart > distance)
+			{
+				SwitchDirection();
+			}
+			transformm.Translate(velocity.x * Time.deltaTime, 0, 0);
+		}
+	}
+	
+	void SwitchDirection()
+	{
+		isGoingRight = !isGoingRight;
+	}
+}
